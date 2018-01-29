4ea27d558d66ea7cf114c8a9a476ca8e8e5f0e46
n.ghribi <n.ghribi@umalis.fr>
2018-01-29 12:39:11 +0100

    update setup.js

--- app/setup.js
+++ app/setup.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 import { Provider } from 'react-redux';
 
 import store from './store'; //Import the store
 import Home from './components/home' //Import the component file
 
-function setup() {
-    class Root extends Component {
-        render() {
-            return (
-                <Provider store={store}>
-                    <Home />
-                </Provider>
-            );
-        }
-    }
-
-    return Root;
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <Provider store={store}>
+          <Home />
+      </Provider>
+    );
+  }
 }
 
-module.exports = setup;
+
 // Redux needs to inject a store holding the app state into the app.
 // To do so, it requires a ‘Provider’ wrapping the whole app.
 // This store is basically a combination of reducers.
